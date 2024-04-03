e7766c427af84ae16cd64687bf5fd398aadeb3e2
luccavilela <luccaloyale@gmail.com>
2024-04-03 17:04:01 -0300

    add crud modelo

---
+++ SGA/src/main/java/bananaScript/SGA/repositorios/ModeloRepositorio.java
@@ -0,0 +1,9 @@
+package bananaScript.SGA.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import bananaScript.SGA.entidades.Modelo;
+
+public interface ModeloRepositorio extends JpaRepository<Modelo, Long>{
+
+}
